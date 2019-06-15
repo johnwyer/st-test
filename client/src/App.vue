2d7bbec23bab25584a24c506bfcae525090b1d87
alexander_7 <alexx.md@gmail.com>
2019-06-16 01:09:41 +0300

    mods

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <app-filter 
-      :app-filters.sync="filter" 
+      :app-filters="filter" 
       @filterChange="filterData"
       @filterReset="filterReset"
     ></app-filter>
